03021310f2f2261e466578eefc1d3f3f9aeb4a32
joycekho <joyceh122@gmail.com>
2017-11-16 21:07:23 -0800

    added linking capability in events, must be specified by {text to display}<link>

--- src/screens/events/event.js
+++ src/screens/events/event.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
   TouchableOpacity, StatusBar, Platform,
-  ScrollView,
+  ScrollView, Linking,
 } from 'react-native';
 import moment from 'moment';
-import { Icon, Divider, Title, View, Screen, Text, Caption } from '@shoutem/ui';
+import { Icon, Divider, Title, View, Screen, Text, Caption, Button } from '@shoutem/ui';
 
 export default class Event extends React.Component {
   static navigationOptions = ({ navigation }) => ({
@@ -21,8 +21,26 @@ export default class Event extends React.Component {
     headerTitleStyle: { alignSelf: 'center', fontFamily: 'Akkurat-Regular', fontSize: 15, color: '#222222', lineHeight: 18 },
   })
 
+parseAndfindURLs(summary) {
+  let url_display = summary.slice(summary.search("{") + 1, summary.search("}"));
+  let begin = summary.search("<");
+  let end = summary.search(">") + 1;
+  let url = '';
+  if (begin != -1){
+    let url = summary.slice(begin + 1, end - 1);
+    summary = summary.slice(0, begin - url_display.length - 2) + summary.slice(end);
+    console.log(url);
+    console.log(url_display);
+    return [url_display,url, summary];
+  }
+else{
+    console.log('not found');
+}
+ return (['','', summary]);
+}
   render() {
     const { event: { enddate, title, startdate, location, summary } } = this.props.navigation.state.params;
+    const description = this.parseAndfindURLs(summary);
     return (
       <Screen>
         <Divider />
@@ -43,8 +61,13 @@ export default class Event extends React.Component {
           { summary &&
             <View style={{ backgroundColor: 'white', paddingTop: 35, paddingHorizontal: 35, paddingBottom: 50 }}>
               <Text>
-                { summary }
+                {description[2]}
               </Text>
+              { (description[1] != null) &&
+                <Text style={{ color: 'blue' }} onPress={() => Linking.openURL(description[1])} >
+                  {description[0]}
+                </Text>
+              }
             </View>
           }
         </ScrollView>
